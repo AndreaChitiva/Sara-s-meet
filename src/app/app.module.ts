f9e1cf4660202622d55c02724d12e5aebdef6500
katorj-design <75459524+katorj-design@users.noreply.github.com>
2021-02-11 20:48:33 -0500

    Agregué el catalogo de productos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -6,6 +6,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatSliderModule } from '@angular/material/slider';
 import {MatInputModule} from '@angular/material/input';
+import { CardComponent } from './card/card.component';
+import {MatTabsModule} from '@angular/material/tabs';
 
 import { CarruselComponent } from './carrusel/carrusel.component';
 import { FormularioComponent } from './formulario/formulario.component';
@@ -14,14 +16,17 @@ import { FormularioComponent } from './formulario/formulario.component';
   declarations: [
     AppComponent,
     CarruselComponent,
-    FormularioComponent
+    FormularioComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatInputModule,
-    MatSliderModule
+    MatSliderModule,
+    MatTabsModule,
   ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
   bootstrap: [AppComponent]
 })
